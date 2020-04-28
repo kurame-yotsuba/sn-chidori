7fa41e6020460451b330dab13c5100b8a26612e3
Gen TAKAHASHI <kuramechan428@gmail.com>
2020-04-29 01:50:11 +0900

    いろいろ追加

--- Chidori/TimeActionScheduler.cs
+++ Chidori/TimeActionScheduler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,40 +10,43 @@ namespace SwallowNest.Chidori
 	/// <summary>
 	/// 受け取ったタスクを時間順に実行していくクラスです。
 	/// </summary>
-	public class TimeActionScheduler
+	public class TimeActionScheduler : IEnumerable<TimeAction>
 	{
 		#region innner member
 
 		public enum AddError
 		{
+			/// <summary>
+			/// エラーはありません。
+			/// </summary>
 			None,
-			InvalidDateTime,
-		}
 
-		public readonly struct AddResult
-		{
-			public readonly TimeAction? Result;
-			public readonly AddError Error;
+			/// <summary>
+			/// 指定された時刻が過去です。
+			/// </summary>
+			TimeIsPast,
 
-			internal AddResult(TimeAction? result, AddError error)
-			{
-				Result = result;
-				Error = error;
-			}
+			/// <summary>
+			/// 指定された名前は既に使われています。
+			/// </summary>
+			NameIsUsed,
 		}
 
 		#endregion
 
 		#region private member
 
-		//実行待ちのタスクを時間順で格納する辞書
+		// 実行待ちのタスクを時間順で格納する辞書
 		readonly SortedDictionary<DateTime, Queue<TimeAction>> scheduler;
 
-		//スケジューラへのタスクの同時追加防止用
+		// アクション名の辞書
+		readonly Dictionary<string, TimeAction> names;
+
+		// スケジューラへのタスクの同時追加防止用
 		readonly object schedulerSync = new object();
 
 		// スケジューラを稼働させるかどうか
-		bool Repeating => Status switch
+		bool Loop => Status switch
 		{
 			TimeActionSchedulerStatus.Stop => true,
 			TimeActionSchedulerStatus.Running => true,
@@ -59,10 +63,10 @@ namespace SwallowNest.Chidori
 
 				var (time, actionQueue) = scheduler.First();
 
-				//先頭のキューの先頭のタスクを取り出す。
+				// 先頭のキューの先頭のタスクを取り出す。
 				var action = actionQueue.Dequeue();
 
-				//先頭のキューの要素数が0なら削除
+				// 先頭のキューの要素数が0なら削除
 				if (scheduler[time].Count == 0)
 				{
 					scheduler.Remove(time);
@@ -75,17 +79,63 @@ namespace SwallowNest.Chidori
 		#endregion
 
 		/// <summary>
-		/// スケジューラの稼働状態を表します。
-		/// </summary>
-		public TimeActionSchedulerStatus Status { get; set; }
-
-		/// <summary>
 		/// コンストラクタ
 		/// </summary>
 		public TimeActionScheduler()
 		{
 			scheduler = new SortedDictionary<DateTime, Queue<TimeAction>>();
+			names = new Dictionary<string, TimeAction>();
 		}
+
+		#region Collection functions
+
+		/// <summary>
+		/// アクション名を列挙します。
+		/// </summary>
+		public IReadOnlyCollection<string> Names => names.Keys;
+
+		/// <summary>
+		/// スケジューラに登録されているタスクの個数です。
+		/// </summary>
+		public int Count { get; private set; }
+
+		#region Implements of IEnumerable<T>
+
+		/// <summary>
+		/// スケジューラに登録されているアクションを列挙します。
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerator<TimeAction> GetEnumerator()
+		{
+			foreach (var (_, queue) in scheduler)
+			{
+				foreach (TimeAction action in queue)
+				{
+					yield return action;
+				}
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		#endregion
+
+		#endregion
+
+		/// <summary>
+		/// スケジューラの稼働状態を表します。
+		/// </summary>
+		public TimeActionSchedulerStatus Status { get; set; }
+
+		/// <summary>
+		/// 名前に紐付くアクションを返します。
+		/// アクションが見つからなかった場合、nullを返します。
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		public TimeAction? this[string name] => names.ContainsKey(name) ? names[name] : null;
+
+		#region Add functions
 
 		/// <summary>
 		/// スケジューラに関数を追加します。
@@ -94,17 +144,21 @@ namespace SwallowNest.Chidori
 		/// <param name="time"></param>
 		/// <param name="name"></param>
 		/// <returns></returns>
-		public AddResult Add(Action action, DateTime time, string name = "")
+		public AddError Add(Action action, DateTime time, string name)
 		{
 			// 引数チェック
 			if (time < DateTime.Now)
 			{
-				return new AddResult(null, AddError.InvalidDateTime);
+				return AddError.TimeIsPast;
+			}
+			if (names.ContainsKey(name))
+			{
+				return AddError.NameIsUsed;
 			}
 
 			lock (schedulerSync)
 			{
-				TimeAction scheduledAction = new TimeAction(action, Count, name);
+				TimeAction scheduledAction = new TimeAction(action, name);
 
 				//指定の時間に既にタスクが入っている場合、そのタスクのあとに追加
 				if (scheduler.ContainsKey(time))
@@ -118,12 +172,13 @@ namespace SwallowNest.Chidori
 					q.Enqueue(scheduledAction);
 					scheduler[time] = q;
 				}
+				names.Add(name, scheduledAction);
 				Count++;
-				return new AddResult(scheduledAction, AddError.None);
+				return AddError.None;
 			}
 		}
 
-		public int Count { get; private set; }
+		#endregion
 
 		/// <summary>
 		/// スケジューラが次にアクションを実行する時間を返します。
@@ -146,7 +201,7 @@ namespace SwallowNest.Chidori
 		public async Task Run()
 		{
 			Status = TimeActionSchedulerStatus.Running;
-			while (Repeating)
+			while (Loop)
 			{
 				if (Count == 0
 					|| Status == TimeActionSchedulerStatus.Stop
@@ -158,13 +213,10 @@ namespace SwallowNest.Chidori
 				else
 				{
 					TimeAction action = Dequeue();
+					names.Remove(action.Name);
 					action.Invoke();
 				}
 			}
 		}
 	}
-
-	
-
-	
 }
